7baf9f38f08589d12f0f647eb9a7926623129d3b
Maxi Feldman <maximiliano.a.feldman@gmail.com>
2024-03-25 12:14:56 -0300

    Add Animations

--- Assets/Movement/Scripts/CharacterMovement.cs
+++ Assets/Movement/Scripts/CharacterMovement.cs
@@ -7,24 +7,41 @@ namespace Movement
     public class CharacterMovement : MonoBehaviour
     {
         public float speed = 1;
+        public Animator animator;
 
         private Vector3 _desiredDirection = new Vector3(0, 0, 0);
         private float _angle;
+        private bool isWalking;
+
+        private void Start()
+        {
+            isWalking = false;
+        }
+
 
         public void Move(Vector3 vectorMovement)
         {
             _desiredDirection = vectorMovement;
+
+            if(!isWalking)
+            {
+                isWalking = true;
+                animator.SetBool("Walk", true);
+            }
         }
 
         private void Update()
         {
+            if(_desiredDirection == Vector3.zero && isWalking)
+            {
+                isWalking = false;
+                animator.SetBool("Walk", false);
+            }
+
             transform.position += (_desiredDirection * (speed * Time.deltaTime));
             _angle = Vector3.SignedAngle(transform.forward, _desiredDirection, Vector3.up);
-            
-            Debug.Log($"rotation: {transform.rotation.y}");
+
             transform.Rotate(0, _angle * (speed * Time.deltaTime), 0);
-            Debug.Log($"angle: {_angle}");
-            Debug.Log($"rotation after: {transform.rotation.y}");
         }
     }
 }
